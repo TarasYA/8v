2a1ebd1afcefdf08e19118aaf3867dfc6935824d
superuser <megdimka@grr.la>
2018-12-07 18:38:32 +0200

    update

--- load_hw.js
+++ load_hw.js
@@ -15,21 +15,21 @@ $.ajax('/hw/list.json', {
                 }
                 template = xhr.responseText;
                 for (var i = window.list_of_predmets.length - 1; i >= 0; i--) {
-                    $.ajax('/hw/' + window.list_of_predmets[i] + '/desc', {
+                    $.ajax('/hw/' + window.list_of_predmets[window.i] + '/desc', {
                         complete: (xhr, stat) => {
                             if (stat != 'success') {
                                 console.error(stat)
                                 return
                             }
                             description = xhr.responseText
-                            $.ajax('/hw/' + window.list_of_predmets[i] + '/name', {
+                            $.ajax('/hw/' + window.list_of_predmets[window.i] + '/name', {
                                 complete: (xhr, stat) => {
                                     if (stat != 'success') {
                                         console.error(stat)
                                         return
                                     }
                                     name = xhr.responseText
-                                    card = template.replace('TYPE', window.list_of_predmets[i]).replace('TEXT', description).replace('NAME', name)
+                                    card = template.replace('TYPE', window.list_of_predmets[window.i]).replace('TEXT', description).replace('NAME', name)
                                     layout.append(card)
                                 }
                             })
