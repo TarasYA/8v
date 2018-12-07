64f0efd580d79fb60af415cf122ff81155ed20ab
superuser <megdimka@grr.la>
2018-12-07 16:17:12 +0200

    update

--- load_hw.js
+++ load_hw.js
@@ -6,7 +6,7 @@ $.ajax('/hw/list.json', {
             return;
         }
         window.list_of_predmets = JSON.parse(xhr.responseText);
-        console.log(list_of_predmets.length)
+        console.log(window.list_of_predmets.length)
         $.ajax('/card.tmp', {
             complete: (xhr, stat) => {
                 if (stat != 'success') {
@@ -14,22 +14,22 @@ $.ajax('/hw/list.json', {
                     return;
                 }
                 template = xhr.responseText;
-                for (var i = list_of_predmets.length - 1; i >= 0; i--) {
-                    $.ajax('/hw/' + list_of_predmets[i] + '/desc', {
+                for (var i = window.list_of_predmets.length - 1; i >= 0; i--) {
+                    $.ajax('/hw/' + window.list_of_predmets[i] + '/desc', {
                         complete: (xhr, stat) => {
                             if (stat != 'success') {
                                 console.error(stat)
                                 return
                             }
                             description = xhr.responseText
-                            $.ajax('/hw/' + list_of_predmets[i] + '/name', {
+                            $.ajax('/hw/' + window.list_of_predmets[i] + '/name', {
                                 complete: (xhr, stat) => {
                                     if (stat != 'success') {
                                         console.error(stat)
                                         return
                                     }
                                     name = xhr.responseText
-                                    card = template.replace('TYPE', list_of_predmets[i]).replace('TEXT', description).replace('NAME', name)
+                                    card = template.replace('TYPE', window.list_of_predmets[i]).replace('TEXT', description).replace('NAME', name)
                                     layout.append(card)
                                 }
                             })
