3cfe7746a78b7194873369242e7dbf7fe439b900
superuser <megdimka@grr.la>
2018-12-07 16:12:55 +0200

    update

--- load_hw.js
+++ load_hw.js
@@ -1,41 +1,41 @@
 layout = $('.mdl-layout-content')
 $.ajax('/hw/list.json', {
+    complete: (xhr, stat) => {
+        if (stat != 'success') {
+            console.error(stat);
+            return;
+        }
+        list_of_predmets = JSON.parse(xhr.responseText);
+        $.ajax('/card.tmp', {
             complete: (xhr, stat) => {
                 if (stat != 'success') {
                     console.error(stat);
                     return;
                 }
-                list_of_predmets = JSON.parse(xhr.responseText);
-                $.ajax('/card.tmp', {
-                    complete: (xhr, stat) => {
-                        if (stat != 'success') {
-                            console.error(stat);
-                            return;
-                        }
-                        template = xhr.responseText;
-                        for (var i = list_of_predmets.length - 1; i >= 0; i--) {
-                            $.ajax('/hw/' + list_of_predmets[i] + '/desc', {
+                template = xhr.responseText;
+                for (var i = list_of_predmets.length - 1; i >= 0; i--) {
+                    $.ajax('/hw/' + list_of_predmets[i] + '/desc', {
+                        complete: (xhr, stat) => {
+                            if (stat != 'success') {
+                                console.error(stat)
+                                return
+                            }
+                            description = xhr.responseText
+                            $.ajax('/hw/' + list_of_predmets[i] + 'name', {
                                 complete: (xhr, stat) => {
                                     if (stat != 'success') {
                                         console.error(stat)
                                         return
                                     }
-                                    description = xhr.responseText
-                                    $.ajax('/hw/' + list_of_predmets[i] + 'name', {
-                                        complete: (xhr, stat) => {
-                                            if (stat != 'success') {
-                                                console.error(stat)
-                                                return
-                                            }
-                                            name = xhr.responseText
-                                            card = template.replace('TYPE', list_of_predmets[i]).replace('TEXT', description).replace('NAME', name)
-                                            layout.append(card)
-                                        }
-                                    })
+                                    name = xhr.responseText
+                                    card = template.replace('TYPE', list_of_predmets[i]).replace('TEXT', description).replace('NAME', name)
+                                    layout.append(card)
                                 }
                             })
                         }
-                    }
-                })
+                    })
+                }
             }
-        }
+        })
+    }
+})
