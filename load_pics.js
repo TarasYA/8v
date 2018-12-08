08114c173358dd24a4b6212f09e07bc8aaa67607
superuser <megdimka@grr.la>
2018-12-08 15:40:32 +0200

    update

---
+++ load_pics.js
@@ -0,0 +1,17 @@
+type = (new URL(location.href)).searchParams.get("t")
+
+on_count = (type) => {
+    return (xhr, stat) => {
+        if (stat != 'success') {
+            console.error('on_count ' + stat)
+            return
+        }
+        n = parseInt(xhr.responseText)
+        for (i = 1; i <= n; i++) {
+            if (id < 10) id = '0' + id
+            $('#parent').append('<img src="/hw/' + type + '/0' + id + '.png"></img>')
+        }
+    }
+}
+
+$.ajax('/hw/' + type + '/imgc', on_count(type))
